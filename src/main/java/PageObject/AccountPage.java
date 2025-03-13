38177ccbb34b5d7f29bf47a683c49c3cb5e03fcd
Efim Klaus <nuclearmarmot@yandex.ru>
2025-03-13 09:56:45 +0300

    add page object and test class for Account test

--- src/main/java/PageObject/AccountPage.java
+++ src/main/java/PageObject/AccountPage.java
@@ -1,7 +1,25 @@
 package PageObject;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AccountPage {
-    //private static final By
+    private static final By PROFILE_SECTION_TEXT = By.xpath(".//*[@href='/account/profile']");
+    private static final By LOGOUT_BUTTON = By.linkText("Выход");
+        //$x(".//*[text()='Выход']")
+
+    private final WebDriver driver;
+    public AccountPage(WebDriver driver){
+        this.driver=driver;
+    }
+
+    public boolean isPageLoaded(){
+        new WebDriverWait(driver,3).until(ExpectedConditions.presenceOfElementLocated(PROFILE_SECTION_TEXT));
+        return driver.findElement(PROFILE_SECTION_TEXT).isDisplayed();
+    }
+    public void logoutButtonClick(){
+        driver.findElement(LOGOUT_BUTTON).click();
+    }
 }
---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,58 @@
+import PageObject.AccountPage;
+import PageObject.MainPage;
+import client.StellarBurgerClient;
+import io.restassured.response.ValidatableResponse;
+import model.LoginWay;
+import model.Token;
+import model.User;
+import model.WebDriverFactory;
+import net.datafaker.Faker;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import java.util.Locale;
+
+public class AccountTest {
+
+    private Token token;
+    private WebDriver driver;
+    private final StellarBurgerClient client = new StellarBurgerClient();
+
+    @Before
+    public void startBrowser() {
+        driver = WebDriverFactory.createWebDriver();
+        Faker faker = new Faker(new Locale("en"));
+        String email = faker.internet().emailAddress();
+        String name = faker.name().firstName();
+        String password =  faker.bothify("??##??##");
+        User user = new User(email,password,name);
+        ValidatableResponse response = client.createUser(user);
+        token = client.getToken(response);
+        driver.get("https://stellarburgers.nomoreparties.site");
+        JavascriptExecutor exec = (JavascriptExecutor)driver;
+        String scriptAccessToken = String.format("window.localStorage.setItem('accessToken', '%s')",token.getAccessToken());
+        String scriptRefreshToken = String.format("window.localStorage.setItem('refreshToken', '%s')",token.getRefreshToken());
+        exec.executeScript(scriptAccessToken);
+        exec.executeScript(scriptRefreshToken);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+        if (token.getAccessToken()!=null){
+            client.deleteUser(token);
+        }
+    }
+
+    @Test
+    public void AccountPageOpenTest(){
+        MainPage main = new MainPage(driver);
+        main.loginPageOpenWith(LoginWay.MAIN_ACCOUNT);
+        AccountPage accountPage = new AccountPage(driver);
+        boolean result = accountPage.isPageLoaded();
+        Assert.assertTrue(result);
+    }
+}
